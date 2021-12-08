918072d9ce3bdb62dbe6b0b1871fc10852b1f6fd
ikael <ikael@student.21-school.ru>
2021-12-08 05:45:49 +0300

    vector
    started implementing of vector

---
+++ vector.hpp
@@ -0,0 +1,13 @@
+#ifndef VECTOR_IKAEL_H
+# define VECTOR_IKAEL_H
+# include <memory>
+
+namespace ft {
+
+	template<class T, class Allocator = std::allocator<T>> class vector {
+		
+	};
+
+}
+
+#endif
