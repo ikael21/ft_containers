ba797763ff319aa704521ce392fb28c085c15c7e
ikael <ikael@student.21-school.ru>
2021-12-10 09:25:45 +0300

    vector
    added default vector public types

--- vector.hpp
+++ vector.hpp
@@ -1,13 +1,30 @@
-#ifndef VECTOR_IKAEL_H
-# define VECTOR_IKAEL_H
+#ifndef VECTOR_H
+# define VECTOR_H
 # include <memory>
+# include <algorithm>
+# include <vector>
 
 namespace ft {
 
-	template<class T, class Allocator = std::allocator<T>> class vector {
-		
-	};
+
+template <class T, class Allocator = std::allocator<T> >
+class vector {
+
+public:
+
+	typedef T											value_type;
+	typedef Allocator									allocator_type;
+	typedef typename allocator_type::size_type			size_type;
+	typedef typename allocator_type::reference			reference;
+	typedef typename allocator_type::const_reference	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
+
+private:
+
+};
+
 
 }
 
-#endif
+#endif // VECTOR_H
