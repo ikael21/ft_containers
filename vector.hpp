1c866f82e5b516b851ab3d960ab0fb69c8610b8f
ikael <ikael@student.21-school.ru>
2021-12-11 16:21:36 +0300

    vector
    added pointer and const_pointer types

--- vector.hpp
+++ vector.hpp
@@ -13,11 +13,11 @@ public:
 
 	typedef T											value_type;
 	typedef Allocator									allocator_type;
-	typedef typename allocator_type::pointer			pointer;
 	typedef typename allocator_type::size_type			size_type;
 	typedef typename allocator_type::reference			reference;
-	typedef typename allocator_type::const_pointer		const_pointer;
 	typedef typename allocator_type::const_reference	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
 
 	explicit vector(const allocator_type& alloc = allocator_type()) {
 
@@ -38,12 +38,10 @@ public:
 	vector(const vector& other) { *this = other; }
 
 	vector&	operator=(const vector& other) {
-		
+
 	}
 
 protected:
-
-private:
 
 };
 
