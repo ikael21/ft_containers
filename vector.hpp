4f0bf55e537ee3757bfef1dc68f0ebdd60256384
ikael <ikael@student.21-school.ru>
2021-12-12 18:06:26 +0300

    vector
    added default, copy and one more
    with size_type first arg constructors
    
    added two methods
    	size()
    	capacity()

--- vector.hpp
+++ vector.hpp
@@ -1,7 +1,6 @@
 #ifndef VECTOR_H
 # define VECTOR_H
 # include <memory>
-# include <algorithm>
 
 namespace ft {
 
@@ -11,37 +10,60 @@ class vector {
 
 public:
 
-	typedef T											value_type;
-	typedef Allocator									allocator_type;
-	typedef typename allocator_type::size_type			size_type;
-	typedef typename allocator_type::reference			reference;
-	typedef typename allocator_type::const_reference	const_reference;
-	typedef typename allocator_type::pointer			pointer;
-	typedef typename allocator_type::const_pointer		const_pointer;
+	typedef T																value_type;
+	typedef Allocator														allocator_type;
+	typedef typename allocator_type::size_type								size_type;
+	typedef typename allocator_type::reference								reference;
+	typedef typename allocator_type::const_reference						const_reference;
+	typedef typename allocator_type::pointer								pointer;
+	typedef typename allocator_type::const_pointer							const_pointer;
 
-	explicit vector(const allocator_type& alloc = allocator_type()) {
+	explicit vector(const allocator_type& alloc = allocator_type())
+	: _alloc(alloc), _start(nullptr), _end(nullptr), _end_capacity(nullptr) {}
 
+	explicit vector(size_type count, const value_type& value = value_type(),
+					const allocator_type& alloc = allocator_type())
+	: _alloc(alloc) {
+		_start = _alloc.allocate(count);
+		for (std::size_t i = 0; i < count; ++i)
+			_start[i] = value;
+		_end = _start + count;
+		_end_capacity = _start + count;
+		return;
 	}
 
-	explicit vector(size_type count,
-					const value_type& value = value_type(),
-					const allocator_type& alloc = allocator_type()) {
-
-	}
-
+/*
 	template<class InputIt>
 	vector(InputIt first, InputIt last,
 			const allocator_type& alloc = allocator_type()) {
 
 	}
+*/
 
 	vector(const vector& other) { *this = other; }
 
 	vector&	operator=(const vector& other) {
-
+		if (this == &other)
+			return *this;
+		_alloc.deallocate(_start, _end_capacity - _start);
+		_alloc = other._alloc;
+		_start = _alloc.allocate(other.capacity());
+		for (std::size_t i = 0; i < other.size(); ++i)
+			_start[i] = other._start[i];
+		_end = _start + other.size();
+		_end_capacity = _start + other.capacity();
+		return *this;
 	}
 
+	std::size_t	capacity(void) const { return _end_capacity - _start; }
+	std::size_t	size(void) const { return _end - _start; }
+
 protected:
+
+	allocator_type	_alloc;
+	pointer			_start;
+	pointer			_end;
+	pointer			_end_capacity;
 
 };
 
