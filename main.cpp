0da48f000875876466af01d3d1637b716c86963e
ikael <ikael@student.21-school.ru>
2021-12-12 18:02:26 +0300

    main.cpp
    added some testes for my vector comparing to default one

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
-#include <stack>
 #include <vector>
-#include "stack.hpp"
 #include <iostream>
+//#include "vector.hpp"
 
 int	main(void) {
 
 //	will be some tests for my containers
+
+	// std::vector<int>	vec(10);
+	// ft::vector<int>		my_vec;
+
+	// std::cout << "capacity: " << vec.capacity() << std::endl;
+	// std::cout << "size: " << vec.size() << std::endl;
+
+	// std::cout << "my vector capacity: " << my_vec.capacity() << std::endl;
+	// std::cout << "my vector size: " << my_vec.size() << std::endl;
+
+	std::allocator<int>	alloc;
+	int					*ptr;
+
+	ptr = nullptr;
+	alloc.deallocate(ptr, 10);
 }
