4b406023539c0fdeea525f53d78f9e42a8ab8d9d
ikael <ikael@student.21-school.ru>
2021-12-15 13:03:40 +0300

    iterator
    added RandomAccessIterator struct

--- iterators/iterator.hpp
+++ iterators/iterator.hpp
@@ -13,24 +13,10 @@ struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 
-//	Base iterator class
-template<class Category, class T, class Distance = std::ptrdiff_t,
-		class Pointer = T*, class Reference = T&>
-struct iterator {
-
-	typedef Category	iterator_category;
-	typedef T			value_type;
-	typedef Distance	difference_type;
-	typedef Pointer		pointer;
-	typedef Reference	reference;
-
-};
-
-
 //	The type trait class that provides uniform
 //	interface to the properties of Iterator types
 template<class Iterator>
-class iterator_traits {
+struct iterator_traits {
 
 	typedef typename Iterator::difference_type		difference_type;
 	typedef typename Iterator::value_type			value_type;
@@ -43,7 +29,7 @@ class iterator_traits {
 
 //	T* specialization member types
 template<class T>
-class iterator_traits<T*> {
+struct iterator_traits<T*> {
 
 	typedef std::ptrdiff_t					difference_type;
 	typedef T								value_type;
@@ -56,13 +42,95 @@ class iterator_traits<T*> {
 
 //	const T* specialization member types
 template<class T>
-class iterator_traits<const T*> {
+struct iterator_traits<const T*> {
 
 	typedef std::ptrdiff_t					difference_type;
 	typedef T								value_type;
 	typedef const T*						pointer;
 	typedef const T&						reference;
 	typedef ft::random_access_iterator_tag	iterator_category;
+
+};
+
+
+//	Base iterator class
+template<class Category, class T, class Distance = std::ptrdiff_t,
+		class Pointer = T*, class Reference = T&>
+struct iterator {
+
+	typedef Category	iterator_category;
+	typedef T			value_type;
+	typedef Distance	difference_type;
+	typedef Pointer		pointer;
+	typedef Reference	reference;
+
+	iterator() { std::cout << "Base iterator" << std::endl; }
+};
+
+template<class T> struct RandomAccessIterator
+: public ft::iterator<ft::random_access_iterator_tag, T> {
+
+private:
+
+//	own base_iterator type for not writing huge ft::iterator<...>
+	typedef ft::iterator<ft::random_access_iterator_tag, T>	base_iterator;
+
+public:
+
+	typedef typename base_iterator::iterator_category	iterator_category;
+	typedef typename base_iterator::value_type			value_type;
+	typedef typename base_iterator::difference_type		difference_type;
+	typedef typename base_iterator::pointer				pointer;
+	typedef typename base_iterator::reference			reference;
+
+	RandomAccessIterator(void) : _ptr(nullptr) { std::cout << "Random iterator" << std::endl; }
+	RandomAccessIterator(pointer const ptr) : _ptr(ptr) { std::cout << "Random iterator" << std::endl; }
+	RandomAccessIterator(const RandomAccessIterator& other) : _ptr(other._ptr) {}
+	~RandomAccessIterator() {}
+
+	RandomAccessIterator&	operator=(const RandomAccessIterator& other) {
+		if (this != &other)
+			_ptr = other.ptr;
+		return *this;
+	}
+
+	RandomAccessIterator&	operator+=(difference_type n) {
+		_ptr += n;
+		return *this;
+	}
+
+	RandomAccessIterator&	operator-=(difference_type n) {
+		_ptr -= n;
+		return *this;
+	}
+
+	RandomAccessIterator&	operator++(void) { ++_ptr; return *this; }
+	RandomAccessIterator&	operator--(void) { --_ptr; return *this; }
+
+	RandomAccessIterator&	operator++(int) {
+		RandomAccessIterator	copy(*this);
+		++_ptr;
+		return copy;
+	}
+
+	RandomAccessIterator&	operator--(int) {
+		RandomAccessIterator	copy(*this);
+		--_ptr;
+		return copy;
+	}
+
+	RandomAccessIterator	operator+(difference_type n) const { return _ptr + n; }
+	RandomAccessIterator	operator-(difference_type n) const { return _ptr - n; }
+
+	bool	operator!=(const RandomAccessIterator& other) const { return !(_ptr == other._ptr); }
+
+	reference	operator*(void) { return *_ptr; };
+
+
+
+private:
+
+	pointer	_ptr;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
-#include <vector>
 #include <iostream>
-//#include "vector.hpp"
+#include <vector>
+#include "vector.hpp"
+#include <algorithm>
+#include <iterator>
+
+
 
 int	main(void) {
 
-//	will be some tests for my containers
+	ft::vector<int>	vec(10, 10);
 
-	// std::vector<int>	vec(10);
-	// ft::vector<int>		my_vec;
-
-	// std::cout << "capacity: " << vec.capacity() << std::endl;
-	// std::cout << "size: " << vec.size() << std::endl;
-
-	// std::cout << "my vector capacity: " << my_vec.capacity() << std::endl;
-	// std::cout << "my vector size: " << my_vec.size() << std::endl;
-
-	std::allocator<int>	alloc;
-	int					*ptr;
-
-	ptr = nullptr;
-	alloc.deallocate(ptr, 10);
+	std::fill(vec.begin(), vec.end(), 5);
+	for (ft::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << std::endl;
 }
