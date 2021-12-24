09646645b82abe5c05332380e256a937643e9fe3
ikael <https://github.com/ikael21>
2021-12-24 13:04:20 +0900

    iterator.hpp
    deleted RandomAccessIterator

--- iterators/iterator.hpp
+++ iterators/iterator.hpp
@@ -5,7 +5,9 @@
 namespace ft {
 
 
-//	Categories of an iterator. Each tag is an empty type.
+/* ************************* */
+/*   CATEGORIES OF ITERATOR  */
+/* ************************* */
 struct input_iterator_tag {};
 struct output_iterator_tag {};
 struct forward_iterator_tag : public input_iterator_tag {};
@@ -13,8 +15,10 @@ struct bidirectional_iterator_tag : public forward_iterator_tag {};
 struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 
-//	The type trait class that provides uniform
-//	interface to the properties of Iterator types
+/* *************************** */
+/* INTERFACE TO THE PROPERTIES */
+/* OF ITERATOR TYPES           */
+/* *************************** */
 template<class Iterator>
 struct iterator_traits {
 
@@ -27,7 +31,9 @@ struct iterator_traits {
 };
 
 
-//	T* specialization member types
+/* ****************************** */
+/* T* specialization member types */
+/* ****************************** */
 template<class T>
 struct iterator_traits<T*> {
 
@@ -40,7 +46,10 @@ struct iterator_traits<T*> {
 };
 
 
-//	const T* specialization member types
+/* ************************* */
+/* const T* specialization   */
+/* member types              */
+/* ************************* */
 template<class T>
 struct iterator_traits<const T*> {
 
@@ -53,7 +62,9 @@ struct iterator_traits<const T*> {
 };
 
 
-//	Base iterator class
+/* ************************* */
+/*    BASE ITERATOR CLASS    */
+/* ************************* */
 template<class Category, class T, class Distance = std::ptrdiff_t,
 		class Pointer = T*, class Reference = T&>
 struct iterator {
@@ -63,74 +74,6 @@ struct iterator {
 	typedef Distance	difference_type;
 	typedef Pointer		pointer;
 	typedef Reference	reference;
-
-	iterator() { std::cout << "Base iterator" << std::endl; }
-};
-
-template<class T> struct RandomAccessIterator
-: public ft::iterator<ft::random_access_iterator_tag, T> {
-
-private:
-
-//	own base_iterator type for not writing huge ft::iterator<...>
-	typedef ft::iterator<ft::random_access_iterator_tag, T>	base_iterator;
-
-public:
-
-	typedef typename base_iterator::iterator_category	iterator_category;
-	typedef typename base_iterator::value_type			value_type;
-	typedef typename base_iterator::difference_type		difference_type;
-	typedef typename base_iterator::pointer				pointer;
-	typedef typename base_iterator::reference			reference;
-
-	RandomAccessIterator(void) : _ptr(nullptr) { std::cout << "Random iterator" << std::endl; }
-	RandomAccessIterator(pointer const ptr) : _ptr(ptr) { std::cout << "Random iterator" << std::endl; }
-	RandomAccessIterator(const RandomAccessIterator& other) : _ptr(other._ptr) {}
-	~RandomAccessIterator() {}
-
-	RandomAccessIterator&	operator=(const RandomAccessIterator& other) {
-		if (this != &other)
-			_ptr = other.ptr;
-		return *this;
-	}
-
-	RandomAccessIterator&	operator+=(difference_type n) {
-		_ptr += n;
-		return *this;
-	}
-
-	RandomAccessIterator&	operator-=(difference_type n) {
-		_ptr -= n;
-		return *this;
-	}
-
-	RandomAccessIterator&	operator++(void) { ++_ptr; return *this; }
-	RandomAccessIterator&	operator--(void) { --_ptr; return *this; }
-
-	RandomAccessIterator&	operator++(int) {
-		RandomAccessIterator	copy(*this);
-		++_ptr;
-		return copy;
-	}
-
-	RandomAccessIterator&	operator--(int) {
-		RandomAccessIterator	copy(*this);
-		--_ptr;
-		return copy;
-	}
-
-	RandomAccessIterator	operator+(difference_type n) const { return _ptr + n; }
-	RandomAccessIterator	operator-(difference_type n) const { return _ptr - n; }
-
-	bool	operator!=(const RandomAccessIterator& other) const { return !(_ptr == other._ptr); }
-
-	reference	operator*(void) { return *_ptr; };
-
-
-
-private:
-
-	pointer	_ptr;
 
 };
 
