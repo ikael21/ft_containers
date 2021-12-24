83a5a15485abb3872ef1a0804b09a89d0dcdd2eb
ikael <https://github.com/ikael21>
2021-12-24 13:06:15 +0900

    vector.hpp
    Changes:
    _end (pointer) -> _size (size_type)
    _end_capacity (pointer) -> _capacity (size_pointer)

--- vector.hpp
+++ vector.hpp
@@ -28,17 +28,20 @@ public:
 */
 
 	explicit vector(const allocator_type& alloc = allocator_type())
-	: _alloc(alloc), _start(nullptr), _end(nullptr), _end_capacity(nullptr) {}
+	: _alloc(alloc), _start(nullptr), _size(0), _capacity(0) {}
 
 	explicit vector(size_type count, const value_type& value = value_type(),
 					const allocator_type& alloc = allocator_type())
 	: _alloc(alloc) {
-		_start = _alloc.allocate(count);
-		for (std::size_t i = 0; i < count; ++i)
-			_start[i] = value;
-		_end = _start + count;
-		_end_capacity = _start + count;
-		return;
+		if (!count)
+			default_init();
+		else {
+			_start = _alloc.allocate(count);
+			for (size_type i = 0; i < count; ++i)
+				_start[i] = value;
+			_size = count;
+			_capacity = _size;
+		}
 	}
 
 /*
@@ -47,8 +50,18 @@ public:
 */
 
 /* need to change when iterators implementation is done */
-	vector(const vector& other)
-	: _alloc(other._alloc), _start(nullptr) { *this = other; }
+	vector(const vector& other) {
+		_alloc = other._alloc;
+		if (!other.size())
+			default_init();
+		else {
+			_start = _alloc.allocate(other.capacity());
+			for(size_type i = 0; i < other._size; ++i)
+				_start[i] = other._start[i];
+			_size = other._size;
+			_capacity = other._capacity;
+		}
+	}
 
 	~vector(void) { _alloc.deallocate(_start, capacity()); }
 
@@ -58,16 +71,21 @@ public:
 			return *this;
 		_alloc.deallocate(_start, capacity());
 		_alloc = other._alloc;
-		_start = _alloc.allocate(other.capacity());
-		for (std::size_t i = 0; i < other.size(); ++i)
-			_start[i] = other._start[i];
-		_end = _start + other.size();
-		_end_capacity = _start + other.capacity();
+		if (!other.size())
+			default_init();
+		else {
+			_start = _alloc.allocate(other.capacity());
+			for (std::size_t i = 0; i < other.size(); ++i)
+				_start[i] = other._start[i];
+			_size = other._size;
+			_capacity = other._capacity;
+		}
 		return *this;
 	}
 
+
 	/* ***************************** */
-	/*     ELEMENT ACCESS METHODS    */
+	/*         ELEMENT ACCESS        */
 	/* ***************************** */
 	const_reference		operator[](size_type pos) const { return *(_start + pos); }
 	reference			operator[](size_type pos) { return *(_start + pos); }
@@ -86,42 +104,66 @@ public:
 
 	reference			front(void) { return _start[0]; }
 	const_reference		front(void) const { return _start[0]; }
+
 	reference			back(void) { return _start[size() - 1]; }
 	const_reference		back(void) const { return _start[size() - 1]; }
+
 	value_type*			data(void) { return _start; }
 	const value_type*	data(void) const { return _start; }
+
 
 	/* ***************************** */
 	/*         ITERATORS             */
 	/* ***************************** */
-	iterator	begin(void) { return _start; }
-	iterator	end(void) { return _end; }
+	iterator		begin(void) { return _start; }
+	const_iterator	begin(void) const { return _start; }
+
+	iterator		end(void) { return _start + size(); }
+	const_iterator	end(void) const { return _start + size(); }
 
 	/* ***************************** */
 	/*           CAPACITY            */
 	/* ***************************** */
-	bool		empty(void) const { return (_start == _end); }
-	size_type	size(void) const { return (_end - _start); }
+	bool		empty(void) const { return (!size()); }
+	size_type	size(void) const { return _size; }
 	size_type	max_size(void) const { return _alloc.max_size(); }
 
 	void		reserve(size_type new_cap) {
-		if (!(new_cap <= capacity())) {
+		if (new_cap > _capacity) {
 			pointer	_tmp = _alloc.allocate(new_cap);
-			for (std::size_t i = 0; i < size(); ++i)
+			for (size_type i = 0; i < _size; ++i)
 				_tmp[i] = _start[i];
 			_alloc.deallocate(_start, capacity());
 			_start = _tmp;
+			_capacity = new_cap;
 		}
 	}
 
-	std::size_t	capacity(void) const { return (_end_capacity - _start); }
+	size_type	capacity(void) const { return _capacity; }
+
+
+	/* ************************* */
+	/*         MODIFIERS         */
+	/* ************************* */
+	void	push_back(const T& value) {
+		if (size() == capacity())
+			(!capacity()) ? reserve(1) : reserve(capacity() * 2);
+		_start[size()] = value;
+		_size++;
+	}
 
 protected:
 
+	void	default_init(void) {
+		_start = nullptr;
+		_size = 0;
+		_capacity = 0;
+	}
+
 	allocator_type	_alloc;
 	pointer			_start;
-	pointer			_end;
-	pointer			_end_capacity;
+	size_type		_size;
+	size_type		_capacity;
 
 };
 
