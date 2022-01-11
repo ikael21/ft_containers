1d3e265447c46c291c9b9217488d84b952a2f589
ikael <ikael@student.21-school.ru>
2022-01-11 22:10:56 +0900

    tests_includes.hpp
    new header to combine all the tests (will be more later)

---
+++ tests/tests_includes.hpp
@@ -0,0 +1,14 @@
+#ifndef TESTS_INCLUDES_H
+# define TESTS_INCLUDES_H
+
+# include <iostream>
+# include <vector>
+# include <chrono>
+# include <vector>
+# include <map>
+# include <stack>
+
+# include "vector.hpp"
+# include "stack.hpp"
+
+#endif
