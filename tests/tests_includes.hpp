b400b2824a4bf612759a9652f353b2579fda2613
ikael <ikael@student.21-school.ru>
2022-01-12 23:13:17 +0900

    tests_includes.hpp
    added test function prototypes
    moved in
    print_container_items(Cont& c, const char* cont_name)
    template function

--- tests/tests_includes.hpp
+++ tests/tests_includes.hpp
@@ -11,4 +11,24 @@
 # include "vector.hpp"
 # include "stack.hpp"
 
+void	iterator_test(void);
+void	speed_test(void);
+void	run_insert_tests(void);
+
+/* ************************************* */
+/* Container must have iterator system,  */
+/* begin() and end() methods             */
+/* ************************************* */
+template<class Container>
+void	print_container_items(Container& c, const char* which_cont) {
+
+	typedef typename Container::iterator	iter;
+
+	std::cout << which_cont << std::endl;
+	for(iter it = c.begin(); it != c.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << "\n>>>>>END<<<<<\n" << '\n';
+	return;
+}
+
 #endif
