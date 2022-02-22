a29fb83034aea09520366f55f00a56a221628f8d
ikael <ikael@student.21-school.ru>
2022-02-22 13:02:28 +0300

    iterator_test.cpp
    removed test2 as it is useless

--- tests/vector/srcs/iterator_test.cpp
+++ tests/vector/srcs/iterator_test.cpp
@@ -3,13 +3,11 @@
 
 static void	test1(void) {
 
-	std_vector::iterator	iter1;
-	std_vector::iterator	iter2;
 	ft_vector::iterator		iter3;
 	ft_vector::iterator		iter4;
 	std::string				result;
 
-	if (iter1 == iter2 and iter3 == iter4)
+	if (iter3 == iter4)
 		result = GREEN ".";
 	else
 		result = RED "F";
@@ -17,19 +15,6 @@ static void	test1(void) {
 }
 
 static void	test2(void) {
-
-	std_vector::iterator	iter1;
-	ft_vector::iterator		iter4;
-	std::string				result;
-
-	if (iter1 == iter4)
-		result = GREEN ".";
-	else
-		result = RED "F";
-	print << result;
-}
-
-static void	test3(void) {
 
 	ft_vector				ft_vec(10);
 	std_vector::iterator	iter1;
@@ -49,7 +34,7 @@ static void	test3(void) {
 	print << result;
 }
 
-static void	test4(void) {
+static void	test3(void) {
 
 	ft_vector				ft_vec(10);
 	std_vector::iterator	iter1;
@@ -72,7 +57,7 @@ static void	test4(void) {
 void	run_iterator_test(void) {
 
 	print << MAGENTA "ITERATOR TESTS: ";
-	test1(); test2(); test3(); test4();
+	test1(); test2(); test3();
 	print << RESET LF;
 
 }
