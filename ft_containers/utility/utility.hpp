0f72ed297cd6fa53a97c529c18764106d4fb8fc1
ikael <ikael@student.21-school.ru>
2022-02-27 15:02:51 +0300

    utility.hpp
    created own lexicographical_compare
    and equal template functions

---
+++ ft_containers/utility/utility.hpp
@@ -0,0 +1,31 @@
+#ifndef UTILITY_H
+
+namespace ft {
+
+
+template<class InputIter1, class InputIter2>
+bool lexicographical_compare(InputIter1 first1, InputIter1 last1,
+							InputIter2 first2, InputIter2 last2) {
+
+	for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
+		if (*first1 < *first2) return true;
+		if (*first2 < *first1) return false;
+	}
+	return ((first1 == last1) && (first2 != last2));
+}
+
+
+template<class InputIter1, class InputIter2>
+bool	equal(InputIter1 first1, InputIter1 last1,
+			InputIter2 first2) {
+
+	for (; first1 != last1; ++first1, ++first2)
+		if (*first1 != *first2)
+			return false;
+	return true;
+}
+
+
+}
+
+#endif // UTILITY_H
