546256a586d60317f33d0c3a6a52e4771d47dfd3
ikael <ikael@student.21-school.ru>
2022-03-04 14:23:29 +0300

    map
    added member types (not all of them)

--- ft_containers/map.hpp
+++ ft_containers/map.hpp
@@ -17,7 +17,30 @@ template<
 >
 class map {
 
+public:
+
+	typedef Key											key_type;
+	typedef T											mapped_type;
+	typedef ft::pair<const key_type, mapped_type>		value_type;
+	typedef Compare										key_compare;
+/*
+	value_compare
+*/
+	typedef Alloc										allocator_type;
+	typedef typename allocator_type::reference			reference;
+	typedef typename allocator_type::const_reference	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
+/*
+	iterators
+*/
+	typedef typename ft::iterator_traits<iterator>::difference_type	difference_type;
+/*
+	size_type
+*/
+
 };
+
 
 }
 
